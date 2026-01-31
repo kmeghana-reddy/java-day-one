274b38d1b25a3f0155bf41371582ec3d3b6a0bbe
kmeghana-reddy <meghanak2511@gmail.com>
2026-01-31 15:09:32 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class New{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your age");
+        int age=sc.nextInt();
+        System.out.println("you are "+age+" years old");
+        sc.close();
+    }
+}   
+    
