b5e0139cbb19f6dd0e766b00265c7379a0f1bb8d
kmeghana-reddy <meghanak2511@gmail.com>
2026-01-31 14:25:30 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,7 @@
+class demo{
+    public static void main(String[] args) {
+        System.out.print("hello");
+        System.out.println(" have a good day");
+        System.out.println();
+    }
+}    
