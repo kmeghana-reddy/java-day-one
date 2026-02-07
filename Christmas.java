f01d627810613b79eeb1dab78ff4ec7eb198169c
kmeghana-reddy <meghanak2511@gmail.com>
2026-02-07 15:59:41 +0530

    Add files via upload

---
+++ Christmas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Christmas{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int day=sc.nextInt();
+        if(day==25){
+            System.out.println("CHRISTMAS");
+        }
+        else{
+            System.out.println("ORDINARY");
+        }   
+        sc.close();
+    }
+}
+        
+        
+        
+    
+
